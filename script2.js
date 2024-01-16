d399d93a5eff06c083e77b1f66b930e6465b920a
a1game <150701776+a1game@users.noreply.github.com>
2024-01-16 15:09:19 -0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the element to be animated
+    var an = document.getElementById('top');
+
+    // Set the width to 10% after a short delay
+    setTimeout(function() {
+an.style.width = "35%"
+    }, 5); // Adjust the delay as needed
+});
